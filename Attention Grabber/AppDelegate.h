0c7c039726b38fb407f2f4523b024e0288852972
Rory Mulligan <rorticus@gmail.com>
2017-02-17 19:51:55 -0500

    Initial Commit

---
+++ Attention Grabber/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Attention Grabber
+//
+//  Created by Rory Mulligan on 2/17/17.
+//  Copyright © 2017 Rory Mulligan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Attention Grabber/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Attention Grabber
+//
+//  Created by Rory Mulligan on 2/17/17.
+//  Copyright © 2017 Rory Mulligan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
